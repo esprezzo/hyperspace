e2c5a5727bbd8b62cc37539f12bd548d79ed2954
alanwilhelm <ajwilhelm@protonmail.com>
2023-02-13 00:03:44 -0800

    Create genesis txns

--- scripts/testnet/3_node_2_create_validator_keys.sh
+++ scripts/testnet/3_node_2_create_validator_keys.sh
@@ -2,3 +2,5 @@
 
 # on node/validator 2 (awspp)
 hyperspaced keys add validator2
+
+# cp genesis.json ~/.hyperspaced/config/
---
+++ scripts/testnet/6_gentx_stuff.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+
+# hyperspaced gentx validator-one 9000000000stake --chain-id testnet-1
+# hyperspaced gentx validator2 1000000000stake --chain-id testnet-1
